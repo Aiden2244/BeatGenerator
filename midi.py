8d277228c2e87683c3812cdb2976f2c9b0a255e9
Aiden2244 <aidenm2244@gmail.com>
2022-11-03 12:15:19 -0400

    The code is now distributed among several files for better organization, and there is now documentation for me to better remember what this code actually does when I inevitably forget in a week or so.

---
+++ midi.py
@@ -0,0 +1,143 @@
+"""
+midi.py
+created by Aiden McCormack on 11-03-2022
+
+This file initializes the midi file that gets written to output, and defines some procedures
+for generating chords & scales as well as some housekeeping procedures to make this thing work.
+"""
+
+from random import randint
+from music import *
+from midiutil.MidiFile import MIDIFile
+
+# initialize the midifile object with 7 tracks
+midi = MIDIFile(7)
+midi.addTrackName(0, 0, "Bass")
+midi.addTrackName(1, 0, "Chords")
+midi.addTrackName(2, 0, "Kick")
+midi.addTrackName(3, 0, "Snare")
+midi.addTrackName(4, 0, "Hi_hat")
+midi.addTrackName(5, 0, "Arpeggio")
+midi.addTrackName(6, 0, "Solo")
+for i in range(7):
+    midi.addTempo(i, 0, TEMPO)
+
+
+# print some info about the track, since it is randomly generated
+def info():
+    print("Key = " + KEYNAME + " " + MODENAME)
+    print("Tempo = " + str(TEMPO))
+
+
+# makes the syntax for writing the file out a little nicer, useful if I ever
+# want to write more than one midifile for some reason
+def write(filename="output.mid"):
+    with open(filename, 'wb') as outf:
+        midi.writeFile(outf)
+
+
+# code that outputs a scale. It was the first thing I wrote, and I might remove it since it's buggy.
+def output_scale(scale, beat=0):
+    for i in range(len(scale)):
+        pitch = ROOT + scale[i]
+        midi.addNote(1, 0, pitch, i+beat, 1, 100)
+    print("output: " + str(scale))
+
+
+# adds a chords to the file
+# parameters:
+    # root_note is the root note of the chord, represented by an integer number of musical half-steps
+    # variety is the type of chord, set to major by default
+    # beat is the beat number where the chord should be added, beginning by default
+    # length is the number of beats that each note of the chord is, set to 4 (whole note) by default
+    # track is the track # that the chord gets written to, I designate 1 as the chords track and this shouldn't change
+    # octave is an optional modifier that shifts the chord up or down by an octave
+    # threshold is a value to make the chords have better voicing, any note above it gets lowered down an octave
+def add_chord(root_note=ROOT, variety=triads['maj'], beat=0, length=4, track=1, octave=0, threshold=FIFTH):
+    for i in range(1, len(variety)):
+        pitch = root_note + variety[i] + (OCTAVE*octave)
+        if pitch > (threshold + (OCTAVE*octave)):
+            pitch = pitch - OCTAVE
+        midi.addNote(track, 0, pitch, beat, length, 100)
+
+
+# writes a progression of chords with repeated calls of the add_chord function
+# parameters:
+    # prog_type is the progression to be built, which is a tuple of chord representations (see the note in music.py)
+    # beat dittos above
+    # space is how far apart each chord is written from the previous chord, 4 (1 measure) by default
+    # length dittos above
+    # track dittos above
+    # octave dittos above
+    # threshold dittos above
+def add_progression(prog_type, beat=0, space=4, length=4, track=1, octave=0, threshold=FIFTH):
+    for i in range(len(prog_type)):
+        add_chord(prog_type[i][0], prog_type[i][1], beat, length, track, octave, threshold)
+        beat = beat+space
+
+
+# generate all possible chords based on the chromatic scale and every chord type defined in 'triads' & 'extensions'
+def generate_all_chords(c=chords):
+    print("Generating all chords")
+    for i in range(len(intervals)):
+        root_note = ROOT + i
+        for j in range(len(triads)):
+            triad = triads_enum[j]
+            c.append((root_note, triad))
+            chord = extensions_enum[j]
+            c.append((root_note, chord))
+
+
+# generates all possible triads within a given scale
+# parameters
+    # mode is the mode (major/minor) of the scale to generate.
+        # ie if mode is major then the piece will generate the chords "I ii iii IV V vi viiº" based on ROOT
+    # pentatonic is a boolean that enables/disables the generation to be solely on the pentatonic scale
+    # c is the list storing every generated chord the beat CAN USE, should not change
+def generate_triads_by_scale(mode=MODENAME, pentatonic=False, c=chords):
+    selector = 0
+    if pentatonic:
+        selector = selector + 2
+        print("Pentatonic scale selected")
+    if mode == 'minor':
+        selector = selector + 1
+        print("Generating chords based on minor scale")
+    elif mode == 'major':
+        print("Generating chords based on major scale")
+    blueprint = chord_builder[selector]
+    scale = scales[selector]
+
+    for i in range(len(blueprint)):
+        chord_id = blueprint[i]
+        root_note = ROOT + scale[i]
+        triad = triads[chord_id]
+        c.append((root_note, triad))
+
+
+# writes a chord progression to the chords track
+# parameters
+    # length is the number of chords to add to the progression
+    # mode is the mode (major/minor) for the progression to generate to
+    # force_unique is a bool that makes the progressions a little more interesting
+        # by disallowing two of the same chord consecutively
+    # force_root is a bool that makes the first chord in the progression equal to the root chord
+        # instead of randomly selecting it
+    # p is the list storing every generated chord the beat ACTUALLY USES, should not change
+        # p is a subset of c
+def generate_progression(length=4, mode='all', force_unique=True, force_root=True, p=progression):
+    if len(chords) == 0:
+        if mode == 'all':
+            generate_all_chords()
+        else:
+            generate_triads_by_scale(mode=mode)
+    prev_index = 0
+    for i in range(length):
+        index = randint(0, (len(chords))-1)
+        if (i == 0) and force_root:
+            progression.append(chords[0])
+        elif (prev_index == index) and force_unique:
+            p.append(chords[index-1])
+        else:
+            p.append(chords[index])
+        prev_index = index
+    return p
---
+++ music.py
@@ -0,0 +1,157 @@
+"""
+music.py
+created by Aiden McCormack on 11-03-2022
+
+This file contains some hardcoded musical information that is useful for the entire program
+"""
+
+from random import randint
+
+# dict relating an index to its string representation
+key_id = {
+    0: 'C',
+    1: 'C#',
+    2: 'D',
+    3: 'Eb',
+    4: 'E',
+    5: 'F',
+    6: 'F#',
+    7: 'G',
+    8: 'Ab',
+    9: 'A',
+    10: 'Bb',
+    11: 'B'
+}
+
+# relates an index to a string representation of the mode
+mode_id = {
+    0: "major",
+    1: "minor"
+}
+
+# relates a descriptive string to a corresponding musical interval, in half steps from the root
+intervals = {
+    'root': 0,
+    'm2': 1,
+    'M2': 2,
+    'm3': 3,
+    'M3': 4,
+    'p4': 5,
+    'tritone': 6,
+    'p5': 7,
+    'm6': 8,
+    'M6': 9,
+    'd7': 10,
+    'M7': 11,
+    'OCTAVE': 12
+}
+
+# same as above but more convenient for the program to work with internally
+root = int(0)
+m2 = 1
+M2 = 2
+m3 = 3
+M3 = 4
+p4 = 5
+tritone = 6
+p5 = 7
+m6 = 8
+M6 = 9
+d7 = 10
+M7 = 11
+OCTAVE = intervals['OCTAVE']
+
+# global variables for the entire program
+KEY = randint(0, 11)
+TEMPO = randint(120, 180)
+KEYNAME = key_id[KEY]
+MODE = randint(0, 1)
+MODENAME = mode_id[MODE]
+PROGRESSION_LEN = 16
+
+# Note identifiers
+MIDDLE_C = 60
+ROOT = MIDDLE_C + KEY
+FIFTH = ROOT + p5
+FOURTH = ROOT + p4
+SECOND = ROOT + M2
+if MODE == 0:
+    THIRD = ROOT + M3
+    SIXTH = ROOT + M6
+    SEVENTH = ROOT + M7
+else:
+    THIRD = ROOT + m3
+    SIXTH = ROOT + m6
+    SEVENTH = ROOT + d7
+
+# scale blueprints: construct a scale using these hardcoded values given only a root note
+scales = {
+    0: (root, M2, M3, p4, p5, M6, M7, OCTAVE, "major"),
+    1: (root, M2, m3, p4, p5, m6, d7, OCTAVE, "minor"),
+    2: (root, M2, M3, p5, M6, OCTAVE, "major pentatonic"),
+    3: (root, m3, p4, p5, d7, OCTAVE, "minor pentatonic")
+}
+
+# A bunch of stuff relating to chords
+# Based on 19th century Western European harmonic analysis, aka what most of the internet thinks is "music theory"
+
+# access chord types by scale with a variable name
+chord_types_in_major = ("maj", "min", "min", "maj", "maj", "min", "dim"),
+chords_types_in_minor = ("min", "dim", "maj", "min", "min", "maj", "maj"),
+chords_types_in_major_pentatonic = ("maj", "min", "min", "maj", "min"),
+chords_types_in_minor_pentatonic = ("min", "maj", "min", "min", "maj"),
+
+# access chord types by scale with a tuple index
+chord_builder = (("maj", "min", "min", "maj", "maj", "min", "dim"),
+("min", "dim", "maj", "min", "min", "maj", "maj"),
+("maj", "min", "min", "maj", "min"),
+("min", "maj", "min", "min", "maj"))
+
+# how to build the triads based on a root note
+triads = {
+    "maj": ("major", root, M3, p5),
+    "min": ("minor", root, m3, p5),
+    "aug": ("augmented", root, M3, m6),
+    "dim": ("diminished", root, m3, tritone)
+}
+
+# same as above but relates it to a number instead of a string
+triads_enum = {
+    0: ("major", root, M3, p5),
+    1: ("minor", root, m3, p5),
+    2: ("augmented", root, M3, m6),
+    3: ("diminished", root, m3, tritone)
+}
+
+# spicier chords
+extensions = {
+    "7": ('7', root, M3, p5, d7),
+    "maj7": ('major 7', root, M3, p5, M7),
+    "min7": ('minor 7', root, m3, p5, d7),
+    "dim7": ('diminished 7', root, m3, tritone, M6)
+}
+
+# same as above but relates it to a number instead of a string
+extensions_enum = {
+    0: ('7', root, M3, p5, d7),
+    1: ('major 7', root, M3, p5, M7),
+    2: ('minor 7', root, m3, p5, d7),
+    3: ('diminished 7', root, m3, tritone, M6)
+}
+
+# some hardcoded progressions
+# NOTE: A chord is represented by a tuple of length 2 with structure (<root note>, <formula to build chord from root>)
+progressions_hardcoded = [
+     ((SECOND, extensions['min7']), (FIFTH, extensions['7']), (ROOT, extensions['maj7']), (ROOT, extensions['maj7']),
+      "iim7_V7_Imaj7"),
+     ((ROOT, triads['maj']), (SIXTH, triads['min']), (FOURTH, triads['maj']), (FIFTH, triads['maj']), "I_vi_IV_V"),
+     ((ROOT, triads['maj']), (SIXTH, triads['min']), (SECOND, triads['min']), (FIFTH, triads['maj']), "I_iv_ii_V"),
+     ((FOURTH, triads['maj']), (FIFTH, triads['maj']), (ROOT, triads['maj']), (SIXTH, triads['min']), "IV_V_I_vi"),
+     ((ROOT, triads['maj']), (SECOND, triads['min']), (SIXTH, triads['min']), (FIFTH, triads['maj']), "I_ii_vi_V")
+]
+
+# the list storing every generated chord the beat can use
+chords = []
+
+# the list storing the chords actually being used by the beat
+progression = []
